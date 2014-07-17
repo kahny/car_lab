81f2241b24d51c78d988784c5f7b716c11e1982e
Connie Chang <changchilla@gmail.com>
2014-07-17 14:12:21 -0700

    version with all tests

--- src/car.js
+++ src/car.js
@@ -3,26 +3,27 @@ function Car(make, model, year, color){
   this.model = model;
   this.year = year; 
   this.color = color; 
-  this.previousOwners = [];
+  this.previousOwners = [];     //
   this.state = "off";
-  this.currentOwner = "manufacturer";
+  this.currentOwner = "manufacturer";   // 
   this.passengers = []
+  this.destination = ""
 };
-
+//
 Car.prototype.sale = function(newOwner){
 	this.previousOwners.push(this.currentOwner); 
 	this.currentOwner = newOwner; //doesn't need this because it doesn't reference up there  
 };
-
+//
 Car.prototype.paint = function(newColor){
-	this.color = this.newColor; 
+	this.color = newColor; //this needs to be newColor 
 };
 
-
+//
 Car.prototype.start = function(){
 	this.state = "on"
 }
-
+//
 Car.prototype.off = function(){
 	this.state = "off"
 }
@@ -30,6 +31,7 @@ Car.prototype.off = function(){
 Car.prototype.driveTo = function(destination){
 	if (this.state === "on"){
 		console.log("Driving to " + destination)
+		this.destination = "Driving to " + destination 
 	}	
 }
 
@@ -41,8 +43,11 @@ Car.prototype.park = function(){
 
 Car.prototype.pickUp = function(name){
 	if (this.state === "on"){
-		console.log("driving to pick up " + name);
+		console.log("Driving to pick up " + name);
 		this.passengers.push(name);
+	}
+	else{
+		console.log("turn on your car")
 	}
 }
 
@@ -51,6 +56,7 @@ Car.prototype.dropOff = function(name){
 		for(var i = 0; i<this.passengers.length; i++){
 			if(this.passengers[i] === name){
 			this.passengers.splice(i, 1);
+			console.log("Dropped off " + name)
 			}
 		}
 	}
@@ -62,6 +68,8 @@ Car.prototype.dropOff = function(name){
 
 module.exports = Car;
 
+//exports is an object by default, and it exports the object we're going to be requiring when we require this file.
+//car is a function (specifically a constructor)
 
 
 
--- test/car_test.js
+++ test/car_test.js
@@ -1,7 +1,7 @@
 // test/car_test.js
 
 var Car = require('../src/car.js'),
-  expect = require('chai').expect;
+  expect = require('chai').expect;  //chai is an assertion library 
 
 
 describe('Car', function(){
@@ -13,24 +13,19 @@ describe('Car', function(){
     myCar = new Car('honda', 'civic', 2000, 'silver');
   });
 
-  
-
-  describe('#passengers', function(){
-    it('should initially be empty', function(){
-      expect(myCar.passengers).to.eql([]);
-    });
+  //describe is a group of tests we are trying to write for a feature. The 'it' block will be testing one particular feature 
+  describe('#sale', function(){
+  	it('should move currentOwner to previousOwner and update currentOwner with the new owner ', function(){
+		myCar.sale("Connie");
+		expect(myCar.previousOwners).to.eql(['manufacturer']);
+		expect(myCar.currentOwner).to.equal('Connie')	
+  	})
   });
 
-  describe('#previousOwners', function(){
-    it('should initially be empty', function(){
-      expect(myCar.previousOwners).to.eql([]);    });
-
-  });
-
-
-  describe('#curretOwner', function(){
-    it('should initially be manufacturer', function(){
-      expect(myCar.currentOwner).to.equal('manufacturer');
+  describe('#paint', function(){
+    it('should update the color of myCar', function(){
+      myCar.paint('gold');
+      expect(myCar.color).to.equal('gold');
     });
   });
 
@@ -48,13 +43,64 @@ describe('Car', function(){
   	})
   })
 
-  describe('#sale', function(){
-  	it('should move currentOwner to previousOwner and update currentOwner with the new owner ', function(){
-		myCar.sale("Connie");
-		expect(myCar.previousOwners).to.eql(['manufacturer']);
-		expect(myCar.currentOwner).to.equal('Connie')	
-	  })
-	});
+  describe('driveTo', function(){
+  	it('should drive the car to a given destination', function(){
+  		myCar.start();
+  		myCar.driveTo("Saratoga");
+  		expect(myCar.destination).to.equal('Driving to Saratoga')
+  	})
+  })
+
+  describe('park', function(){
+  	it('should change the car to parked', function(){
+  		myCar.park();
+  		expect(myCar.state).to.equal('off');
+  	})
+  })
+
+ describe('pickUp', function(){
+  	it('should pick up a passenger and add them to passenger list', function(){
+  		myCar.start();
+  		myCar.pickUp("Sally")
+  		expect(myCar.passengers.pop()).to.equal('Sally');
+  		// expect(myCar.passengers).to.eql(["Sally"])    //alternative since there is only one item in the array 
+  	})
+  })
+
+
+ describe('dropOff', function(){
+  	it('should drop off a passenger and take them off the passenger list', function(){
+  		myCar.start();
+  		myCar.pickUp("Sally")
+  		myCar.dropOff("Sally")
+  		expect(myCar.passengers).to.eql([]);
+  	})
+  })
+
+//the parameters called are all unique to the specific describe, just because you picked up sally in the previous describe it doesn't carry over to dropoff test
+
+
+  describe('#passengers', function(){
+    it('should initially be empty', function(){
+      expect(myCar.passengers).to.eql([]);
+    });
+  });
+
+  describe('#previousOwners', function(){
+    it('should initially be empty', function(){
+      expect(myCar.previousOwners).to.eql([]);    
+  	});
+  });
+
+
+  describe('#curretOwner', function(){
+    it('should initially be manufacturer', function(){
+      expect(myCar.currentOwner).to.equal('manufacturer');
+    });
+  });
+
+
+
 
 });
 
